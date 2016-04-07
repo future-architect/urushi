ea30ad3dd6c0407bf1d0b3fb5d3985f2fd862ed9
Yuzo Hirakawa <y.hirakawa.z3@future.co.jp>
2016-04-07 10:51:52 +0900

    TopページへのUrushi組込み完了。

--- gh-pages-resoucres/start.js
+++ gh-pages-resoucres/start.js
@@ -1,21 +1,28 @@
-hljs.initHighlightingOnLoad();
 
-window.onload = function () {
-	var header = document.getElementById('header')
+(function () {
+	'use strict';
+	var header, onscroll;
+	
+	window.hljs.initHighlightingOnLoad();
+	require.config(requireConfig);
 
-	var onscroll = function () {
+	header = document.getElementById('header');
+
+	onscroll = function () {
 		var y = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 		if (y && !header.classList.contains('floating')) {
 			header.classList.add('floating');
 		} else if (!y && header.classList.contains('floating')) {
 			header.classList.remove('floating');
 		}
-	}
+	};
 
 	if (document.addEventListener) {
 		document.addEventListener('scroll', onscroll, false);
 	} else {
 		document.attachEvent('on' + 'scroll', onscroll);
 	}
-	console.log('cear');
-};
+	require(['templateEngine', 'templateConfig'], function (templateEngine, templateConfig) {
+		templateEngine.renderDocument(document.body, templateConfig);
+	});
+})();
