3cdb474e2557ea015c4f8073eeaa9b47494e7289
saito2380 <n.saito.wb@future.co.jp>
2016-07-20 11:43:37 +0900

    css追加、alert追加

---
+++ ja-jp/demo/alert/main.js
@@ -0,0 +1,17 @@
+window.requireConfig.baseUrl = '../../../';
+require.config(requireConfig);
+require(['Urushi','Alert', 'templateEngine', 'templateConfig'], function(Urushi,Alert,templateEngine,
+		templateConfig) {
+	templateEngine.renderDocument(document.body, templateConfig).then(
+			function(result) {
+				var alerts = result.widgets, key;
+				for (key in alerts) {
+					alerts[key].show();
+				}
+				flag = true;
+				done();
+			}).otherwise(function(error) {
+		flag = false;
+		done();
+	});
+});
