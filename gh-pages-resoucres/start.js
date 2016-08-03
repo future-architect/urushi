1d237ef79c019e4a6abccf7a519e5441e6a2bb50
San Yamagami <s.yamagami.ah@future.co.jp>
2016-08-03 14:10:31 +0900

    ヘッダのレスポンシブ対応（スマホやタブレットではコンテキストメニューを表示） 開発者向けページのスタイル崩れ対応

--- gh-pages-resoucres/start.js
+++ gh-pages-resoucres/start.js
@@ -1,8 +1,81 @@
 
 (function () {
 	'use strict';
-	var header, onscroll;
+	var header, onscroll, contextItems, contextItem, localeChangeItem, key, itemCallback, callbackFnc;
+
+	// 画面情報
+	var screens = {
+		'index' : {
+			'id' : 'index',
+			'name' : 'TOP PAGE',
+			'path' : '/',
+			'screenFileNm' : 'index'
+		},
+		'get-started' : {
+			'id' : 'getStarted',
+			'name' : 'GET STARTED',
+			'path' : '/get-started.html',
+			'screenFileNm' : 'get-started'
+		},
+		'for-developers' : {
+			'id' : 'forDevelopers',
+			'name' : 'FOR DEVELOPERS',
+			'path' : '/for-developers.html',
+			'screenFileNm' : 'for-developers'
+		}
+	};
+	var isLocaleJa = false;
+	var LOCALE_JA_JP = 'ja-JP';
+
+	// URLからファイル名取得
+	var screenFileNm = window.location.href.split('/').pop().split('.')[0];
 	
+	// index.htmlの場合
+	if(!screenFileNm) {
+		screenFileNm = 'index';
+	}
+
+	if (window.location.pathname.match(LOCALE_JA_JP)) {
+		isLocaleJa = true;
+	}
+
+	// コンテキストアイテムのコールバック関数を返す
+	itemCallback = function (path, isLocaleJa) {
+		return function() {
+			if(isLocaleJa) {
+				path = '/' + LOCALE_JA_JP + path;
+			}
+			location.href = path;
+		};
+	};
+
+	// ロケール変更アイテム
+	localeChangeItem = {};
+	localeChangeItem.id = screens[screenFileNm].id;
+	if(isLocaleJa) {
+		localeChangeItem.name = 'ENGLISH';
+		localeChangeItem.label = 'ENGLISH';
+	} else {
+		localeChangeItem.name = 'JAPANESE';
+		localeChangeItem.label = 'JAPANESE';
+	}
+	localeChangeItem.callback = itemCallback(screens[screenFileNm].path, !isLocaleJa);
+
+	contextItems = [];
+	for(key in screens) {
+		if(key !== screenFileNm) {
+			callbackFnc = itemCallback(screens[key].path, isLocaleJa);
+			contextItem = {
+				id : screens[key].id,
+				name : screens[key].name,
+				label : screens[key].name,
+				callback : callbackFnc
+			};
+			contextItems.push(contextItem);
+		}
+	}
+	contextItems.push(localeChangeItem);
+
 	require.config(requireConfig);
 
 	header = document.getElementById('header');
@@ -21,7 +94,22 @@
 	} else {
 		document.attachEvent('on' + 'scroll', onscroll);
 	}
+
 	require(['templateEngine', 'templateConfig'], function (templateEngine, templateConfig) {
-		templateEngine.renderDocument(document.body, templateConfig);
+		templateEngine.renderDocument(document.body, templateConfig).then(
+			function (result) {
+				// コンテキストメニュー作成
+				require(['ContextMenu'], function (ContextMenu) {
+					var contextMenu = new ContextMenu({
+						id : 'headerContextMenu',
+						additionalClass : '',
+						items : contextItems
+					});
+					document.querySelector('.headerContextNarrow').appendChild(contextMenu.getRootNode());
+				});
+			}).otherwise(function (error) {
+				console.log(error);
+			}
+		);
 	});
 })();
