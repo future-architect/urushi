174f9e30442d30e5204fdb0b0379e4b72302f28f
saito2380 <n.saito.wb@future.co.jp>
2016-08-04 14:52:26 +0900

    ヘッダcss等修正、デモページ追加、メディアクエリ追加

---
+++ ja-jp/demo/contextmenu/main.js
@@ -0,0 +1,7 @@
+window.requireConfig.baseUrl = '../../../../';
+
+require.config(requireConfig);
+require([ 'templateEngine', 'templateConfig' ], function(templateEngine,
+		templateConfig) {
+	templateEngine.renderDocument(document.body, templateConfig);
+});
