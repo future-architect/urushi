69c19f3408818bd4918ff4116db9cf709c2f7808
Yuzo.H <y.hirakawa.z3@future.co.jp>
2017-03-17 14:33:02 +0900

    コーディング規約対応

--- util/node/gulp/tasks/javascript.js
+++ util/node/gulp/tasks/javascript.js
@@ -6,21 +6,16 @@ var gulp = require('gulp'),
 gulp.task('js-build', function() {
 	'use strict';
 	var i, length;
+
 	for (i = 0, length = config.js.src.rjs.length; i < length; i++) {
-		gulp.src(config.js.src.rjs[i])
-			.pipe(rjs(config.js.rjs))
-			.pipe(uglify())
-			.pipe(gulp.dest(config.js.dest.rjs[i]));
+		gulp.src(config.js.src.rjs[i]).pipe(rjs(config.js.rjs)).pipe(uglify()).pipe(gulp.dest(config.js.dest.rjs[i]));
 	}
 
 	for (i = 0, length = config.js.src.uglify.length; i < length; i++) {
-		gulp.src(config.js.src.uglify[i])
-			.pipe(uglify())
-			.pipe(gulp.dest(config.js.dest.uglify[i]));
+		gulp.src(config.js.src.uglify[i]).pipe(uglify()).pipe(gulp.dest(config.js.dest.uglify[i]));
 	}
 
 	for (i = 0, length = config.js.src.trans.length; i < length; i++) {
-		gulp.src(config.js.src.trans[i])
-			.pipe(gulp.dest(config.js.dest.trans[i]));
+		gulp.src(config.js.src.trans[i]).pipe(gulp.dest(config.js.dest.trans[i]));
 	}
 });
