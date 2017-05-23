97c0ec17c887181835852349f82db40fbcea5f52
Yuzo.H <y.hirakawa.z3@future.co.jp>
2017-05-23 19:25:35 +0900

    Checkboxに対して下記を実践。 ・他ライブラリへの依存性排除 ・CSS3.0未サポートブラウザ用コードを削除

--- test/checkbox/spec.js
+++ test/checkbox/spec.js
@@ -1,19 +1,19 @@
 define(
 	'checkbox.spec',
 	['Urushi', 'Checkbox', 'templateEngine', 'templateConfig'],
-	function (Urushi, Checkbox, templateEngine, templateConfig) {
+	function(Urushi, Checkbox, templateEngine, templateConfig) {
 		'use strict';
 		var temp,
-			hasTransitionSupportTrue = function () {return true; },
-			hasTransitionSupportFalse = function () {return false; };
+			hasTransitionSupportTrue = function() { return true; },
+			hasTransitionSupportFalse = function() { return false; };
 
-		describe('Checkbox test', function () {
+		describe('Checkbox test', function() {
 			var checkbox,
 				legacyCheckbox,
 				parentNode = document.getElementById('script-modules');
 
-			beforeEach(function(){
-				var cb = new Checkbox({label : 'optional label'});
+			beforeEach(function() {
+				var cb = new Checkbox({label: 'optional label'});
 				cb.destroy();
 
 				checkbox = new Checkbox();
@@ -25,13 +25,13 @@ define(
 				parentNode.appendChild(legacyCheckbox.getRootNode());
 				Urushi.hasTransitionSupport = temp;
 			});
-			it('getValue', function () {
+			it('getValue', function() {
 				expect(checkbox.getValue()).toBe(false);
 				checkbox.inputNode.checked = true;
 				expect(checkbox.getValue()).toBe(true);
 				checkbox.inputNode.checked = false;
 			});
-			it('setChecked', function () {
+			it('setChecked', function() {
 				expect(checkbox.setChecked()).toBe();
 
 				temp = Urushi.hasTransitionSupport;
@@ -45,54 +45,42 @@ define(
 
 				Urushi.hasTransitionSupport = temp;
 			});
-			it('clear', function () {
+			it('clear', function() {
 				expect(checkbox.setChecked(true)).toBe();
 				expect(checkbox.clear()).toBe();
 
 				expect(checkbox.getValue()).toBe(false);
 
 			});
-			it('_onClick', function () {
-				temp = Urushi.hasTransitionSupport;
-				Urushi.hasTransitionSupport = hasTransitionSupportTrue;
-				expect(checkbox._onClick()).toBe();
-				expect(checkbox._onClick()).toBe();
-
-				Urushi.hasTransitionSupport = hasTransitionSupportFalse;
-				expect(legacyCheckbox._onClick()).toBe();
-				expect(legacyCheckbox._onClick()).toBe();
-
-				Urushi.hasTransitionSupport = temp;
-			});
-			it('getPropertyValue, setPropertyValue', function () {
+			it('getPropertyValue, setPropertyValue', function() {
 				expect(checkbox.getPropertyValue()).toBe('on');
 				expect(checkbox.setPropertyValue('test')).toBe();
 				expect(checkbox.getPropertyValue('test')).toBe('test');
 			});
-			it('setDisabled', function () {
+			it('setDisabled', function() {
 				expect(checkbox.setDisabled()).toBe(false);
 				expect(checkbox.setDisabled(true)).toBe(true);
 				expect(checkbox.setDisabled(true)).toBe(true);
 				expect(checkbox.setDisabled(false)).toBe(true);
 				expect(checkbox.setDisabled(false)).toBe(true);
 			});
-			it('destroy', function () {
+			it('destroy', function() {
 				expect(checkbox.destroy()).toBe();
 				expect(legacyCheckbox.destroy()).toBe();
 			});
 
-			describe('Template engine', function () {
+			describe('Template engine', function() {
 				var flag = false;
-				beforeEach(function (done) {
-					templateEngine.renderDocument(document.body, templateConfig).then(function (result) {
+				beforeEach(function(done) {
+					templateEngine.renderDocument(document.body, templateConfig).then(function(result) {
 						flag = true;
 						done();
-					}).otherwise(function (error) {
+					}).otherwise(function(error) {
 						flag = false;
 						done();
 					});
 				});
-				it('template engine test', function () {
+				it('template engine test', function() {
 					expect(flag).toBe(true);
 				});
 			});
--- urushi/material/js/Checkbox.js
+++ urushi/material/js/Checkbox.js
@@ -58,6 +58,7 @@
 define(
 	'Checkbox',
 	[
+		'jquery',
 		'Urushi',
 		'materialConfig',
 		'_Base',
@@ -71,7 +72,7 @@ define(
 	 * @alias module:Checkbox
 	 * @returns {object} Checkbox instance.
 	 */
-	function(urushi, materialConfig, _Base, template, transitionUnit, rippleTransitionUnit) {
+	function($, urushi, materialConfig, _Base, template, transitionUnit, rippleTransitionUnit) {
 		'use strict';
 
 		/**
@@ -83,21 +84,7 @@ define(
 		 */
 		var CONSTANTS = {
 			ID_PREFIX: 'urushi.checkbox',
-			EMBEDDED: {checkboxClass: '', additionalClass: '', label: ''},
-			INTERVAL: 20,
-			RIPPLE_SCALE_MIN: 0,
-			RIPPLE_SCALE_MAX: 1,
-			CHECK_DEFAULT_TOP: 14,
-			CHECK_DEFAULT_LEFT: -3,
-			CHECK_DEFAULT_WIDTH: 0,
-			CHECK_DEFAULT_HIGHT: 0,
-			CHECK_ANIMATE_WIDTH_TOP: 13,
-			CHECK_ANIMATE_WIDTH_LEFT: -5,
-			CHECK_ANIMATE_WIDTH_WIDTH: 7,
-			CHECK_ANIMATE_WIDTH_HIGHT: 2,
-			CHECK_ANIMATE_HIGHT_TOP: 4,
-			CHECK_ANIMATE_HIGHT_LEFT: -1,
-			CHECK_ANIMATE_HIGHT_HIGHT: 13
+			EMBEDDED: {checkboxClass: '', additionalClass: '', label: ''}
 		};
 
 		/**
@@ -148,24 +135,6 @@ define(
 			},
 			/**
 			 * <pre>
-			 * The browser not support CSS3.0,
-			 * it creates element nodes there have check animation.
-			 * </pre>
-			 * @protected
-			 * @param {object} args Constructor arguments.
-			 * @returns none.
-			 */
-			_render: function(args) {
-				this._super(args);
-
-				if (urushi.hasTransitionSupport()) {
-					return;
-				}
-				$(this.rootNode).find('.ripple').replaceWith(rippleTransitionUnit);
-				$(this.rootNode).find('.check').replaceWith(transitionUnit);
-			},
-			/**
-			 * <pre>
 			 * Show close icon.
 			 * Set checkbox initialize value.
 			 * The browser not support CSS3.0,
@@ -177,11 +146,6 @@ define(
 			 */
 			initOption: function(/* object */ args) {
 				this.setChecked(args.checked);
-
-				if (urushi.hasTransitionSupport()) {
-					return;
-				}
-				urushi.addEvent(this.inputNode, 'click', this, '_onClick');
 			},
 			/**
 			 * <pre>
@@ -211,101 +175,11 @@ define(
 			 * @returns none.
 			 */
 			setChecked: function(/* boolean */ is) {
-				var checkDuration,
-					checkNode;
-
 				if ('boolean' !== typeof is) {
 					return;
 				}
 
 				this.inputNode.checked = is;
-
-				if (urushi.hasTransitionSupport()) {
-					return;
-				}
-
-				if (is) {
-					checkDuration = this.duration * 0.5;
-					checkNode = this.checkNode;
-
-					$(this.checkNode).css({
-						top: CONSTANTS.CHECK_DEFAULT_TOP,
-						left: CONSTANTS.CHECK_DEFAULT_LEFT,
-						width: CONSTANTS.CHECK_DEFAULT_WIDTH,
-						height: CONSTANTS.CHECK_DEFAULT_HIGHT,
-						opacity: materialConfig.DEFAULT_VALUE_OPACITY_MAX,
-					});
-
-					$(this.checkBackgroundNode).animate({
-						opacity: materialConfig.DEFAULT_VALUE_OPACITY_MAX
-					}, checkDuration);
-
-					$(this.checkBackgroundNode).css({
-						opacity: materialConfig.DEFAULT_VALUE_OPACITY_MAX
-					});
-
-					$(checkNode).animate({
-						top: CONSTANTS.CHECK_ANIMATE_WIDTH_TOP,
-						left: CONSTANTS.CHECK_ANIMATE_WIDTH_LEFT,
-						width: CONSTANTS.CHECK_ANIMATE_WIDTH_WIDTH,
-						hight: CONSTANTS.CHECK_ANIMATE_WIDTH_HIGHT
-					}, checkDuration, function() {
-						$(checkNode).animate({
-							top: CONSTANTS.CHECK_ANIMATE_HIGHT_TOP,
-							left: CONSTANTS.CHECK_ANIMATE_HIGHT_LEFT,
-							height: CONSTANTS.CHECK_ANIMATE_HIGHT_HIGHT
-						}, checkDuration);
-					});
-				} else {
-					$(this.checkBackgroundNode).animate({
-						opacity: materialConfig.DEFAULT_VALUE_OPACITY_MIN
-					}, this.duration);
-
-					$(this.checkNode).animate({
-						opacity: materialConfig.DEFAULT_VALUE_OPACITY_MIN
-					}, this.duration);
-				}
-			},
-			/**
-			 * <pre>
-			 * Callback function for checkbox is clicked.
-			 * It used in the browser not support CSS3.0.
-			 * </pre>
-			 * @param {object} event Event object.
-			 * @returns none.
-			 */
-			_onClick: function(/* object */ event) {
-				var rippleCounter = 0,
-					rippleScale = CONSTANTS.RIPPLE_SCALE_MIN,
-					opacityMin = materialConfig.DEFAULT_VALUE_RIPPLE_OPACITY_MIN,
-					opacityMax = materialConfig.DEFAULT_VALUE_RIPPLE_OPACITY_MAX,
-					rippleOpacity = opacityMin,
-					ripple = function(/* object */ rippleNode, /* number */ duration) {
-						if (rippleCounter * CONSTANTS.INTERVAL < duration) {
-							setTimeout(function() {
-								rippleScale += CONSTANTS.RIPPLE_SCALE_MAX * CONSTANTS.INTERVAL / duration;
-								rippleOpacity += opacityMax * CONSTANTS.INTERVAL / duration;
-								$(rippleNode).css({
-									'msTransform': 'scale(' + rippleScale + ')',
-									'opacity': rippleOpacity
-								});
-								rippleCounter++;
-								ripple(rippleNode, duration);
-							}, CONSTANTS.INTERVAL);
-						} else {
-							$(rippleNode).animate({
-								'msTransform': 'scale(' + CONSTANTS.RIPPLE_SCALE_MIN + ')',
-								'opacity': opacityMin
-							}, duration - 100);
-						}
-					};
-
-				if (urushi.hasTransitionSupport()) {
-					return;
-				}
-
-				ripple(this.rippleNode, this.duration);
-				this.setChecked(this.inputNode.checked);
 			},
 			/**
 			 * <pre>
@@ -322,14 +196,6 @@ define(
 			 */
 			_attachNode: function() {
 				this.inputNode = this.rootNode.getElementsByTagName('input')[0];
-
-				if (urushi.hasTransitionSupport()) {
-					return;
-				}
-
-				this.rippleNode = this.rootNode.getElementsByClassName('checkbox-ripple-unit')[0];
-				this.checkNode = this.rootNode.getElementsByClassName('checkbox-check-unit')[0];
-				this.checkBackgroundNode = this.rootNode.getElementsByClassName('checkbox-check-background')[0];
 			},
 			/**
 			 * @see {@link module:_Base}#_getId
@@ -379,20 +245,6 @@ define(
 				}
 				return true;
 			},
-			/**
-			 * <pre>
-			 * Discarding of instance.
-			 * Delete Callback functions.
-			 * </pre>
-			 * @returns none.
-			 */
-			destroy: function() {
-				if (!urushi.hasTransitionSupport()) {
-					urushi.removeEvent(this.inputNode, 'click', this, '_onClick');
-				}
-
-				this._super();
-			}
 		});
 	}
 );
