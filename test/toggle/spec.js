61a3b3da6895e184d3ce569282eb211d71fe6c4c
Yuzo.H <y.hirakawa.z3@future.co.jp>
2017-05-26 17:48:44 +0900

    #9 toggleのjquery依存を削除

--- test/toggle/spec.js
+++ test/toggle/spec.js
@@ -1,107 +1,51 @@
 define(
 	'toggle.spec',
 	['Urushi', 'ToggleButton', 'templateEngine', 'templateConfig'],
-	function (Urushi, ToggleButton, templateEngine, templateConfig) {
+	function(Urushi, ToggleButton, templateEngine, templateConfig) {
 		'use strict';
-		var temp,
-			hasTransitionSupportTrue = function () {return true; },
-			hasTransitionSupportFalse = function () {return false; };
 
-		describe('Toggle test', function () {
+		describe('Toggle test', function() {
 			var toggle1,
-				toggle2,
-				toggle3,
 				parentNode = document.getElementById('script-modules');
 
-			it('init', function () {
+			it('init', function() {
 				expect((new ToggleButton()).destroy()).toBe();
 
-				temp = Urushi.hasTransitionSupport;
-				Urushi.hasTransitionSupport = hasTransitionSupportTrue;
-				toggle1 = new ToggleButton({id : 'toggle1'});
+				toggle1 = new ToggleButton({id: 'toggle1'});
 				parentNode.appendChild(toggle1.getRootNode());
-
-				Urushi.hasTransitionSupport = hasTransitionSupportFalse;
-				toggle2 = new ToggleButton({id : 'toggle2'});
-				toggle3 = new ToggleButton({id : 'toggle3', toggleButtonClass : 'toggle-primary'});
-				parentNode.appendChild(toggle2.getRootNode());
-
-				Urushi.hasTransitionSupport = temp;
 			});
-			it('setDisabled', function () {
+			it('setDisabled', function() {
 				expect(toggle1.setDisabled()).toBe(false);
 				expect(toggle1.setDisabled(true)).toBe(true);
 				expect(toggle1.setDisabled(false)).toBe(true);
 			});
-			it('_onClick', function () {
-				temp = Urushi.hasTransitionSupport;
-				Urushi.hasTransitionSupport = hasTransitionSupportTrue;
-				expect(toggle1._onClick()).toBe();
-
-				Urushi.hasTransitionSupport = hasTransitionSupportFalse;
-				expect(toggle2._onClick()).toBe();
-				expect(toggle2._onClick()).toBe();
-				toggle2.setDisabled(true);
-				expect(toggle2._onClick()).toBe();
-				toggle2.setDisabled(false);
-				expect(toggle2._onClick()).toBe();
-
-				Urushi.hasTransitionSupport = temp;
-			});
-			it('getValue, setValue', function () {
-				temp = Urushi.hasTransitionSupport;
-				Urushi.hasTransitionSupport = hasTransitionSupportTrue;
+			it('getValue, setValue', function() {
 				expect(toggle1.setValue(false)).toBe();
 				expect(toggle1.getValue()).toBe(false);
 				expect(toggle1.setValue(true)).toBe();
 				expect(toggle1.getValue()).toBe(true);
-
-				Urushi.hasTransitionSupport = hasTransitionSupportFalse;
-				toggle2.setDisabled(false);
-				toggle3.setDisabled(false);
-				expect(toggle2.setValue(false)).toBe();
-				expect(toggle2.getValue()).toBe(false);
-				expect(toggle2.setValue(true)).toBe();
-				expect(toggle2.getValue()).toBe(true);
-
-				expect(toggle3.setValue(false)).toBe();
-				expect(toggle3.getValue()).toBe(false);
-				expect(toggle3.setValue(true)).toBe();
-				expect(toggle3.getValue()).toBe(true);
-
-				Urushi.hasTransitionSupport = temp;
 			});
-			it('clear', function () {
+			it('clear', function() {
 				expect(toggle1.setValue(true)).toBe();
 				expect(toggle1.clear()).toBe();
 				expect(toggle1.getValue()).toBe(false);
-
-				Urushi.hasTransitionSupport = temp;
 			});
-			it('destroy', function () {
-				temp = Urushi.hasTransitionSupport;
-				Urushi.hasTransitionSupport = hasTransitionSupportTrue;
+			it('destroy', function() {
 				expect(toggle1.destroy()).toBe();
-
-				Urushi.hasTransitionSupport = hasTransitionSupportFalse;
-				expect(toggle2.destroy()).toBe();
-				expect(toggle3.destroy()).toBe();
-
-				Urushi.hasTransitionSupport = temp;
 			});
 
-			describe('Template engine', function () {
+			describe('Template engine', function() {
 				var flag = false;
-				beforeEach(function (done) {
-					templateEngine.renderDocument(document.body, templateConfig).then(function (result) {
+				beforeEach(function(done) {
+					templateEngine.renderDocument(document.body, templateConfig).then(function(result) {
 						flag = true;
 						done();
-					}).otherwise(function (error) {
+					}).otherwise(function(error) {
 						flag = false;
 						done();
 					});
 				});
-				it('template engine test', function () {
+				it('template engine test', function() {
 					expect(flag).toBe(true);
 				});
 			});
--- urushi/material/js/ToggleButton.js
+++ urushi/material/js/ToggleButton.js
@@ -59,8 +59,6 @@
 define(
 	'ToggleButton',
 	[
-		'jquery',
-		'jqueryUi',
 		'Urushi',
 		'materialConfig',
 		'_Base',
@@ -73,7 +71,7 @@ define(
 	 * @alias module:ToggleButton
 	 * @returns {object} ToggleButton object.
 	 */
-	function($, jqueryUi, Urushi, materialConfig, _Base, template, transitionUnit) {
+	function(Urushi, materialConfig, _Base, template, transitionUnit) {
 		'use strict';
 		/**
 		 * <pre>
@@ -84,14 +82,7 @@ define(
 		 */
 		var CONSTANTS = {
 			ID_PREFIX: 'urushi.ToggleButton',
-			EMBEDDED: {toggleButtonClass: '', additionalClass: '', label: ''},
-			INTERVAL: 10,
-			RIPPLE_SCALE_MIN: 0,
-			RIPPLE_SCALE_MAX: 1,
-			CHECKMARK_POSITION_LEFT: 15,
-			CHECKMARK_POSITION_RIGHT: -5,
-			RIPPLE_POSITION_LEFT: 2,
-			RIPPLE_POSITION_RIGHT: -18
+			EMBEDDED: {toggleButtonClass: '', additionalClass: '', label: ''}
 		};
 
 		/**
@@ -131,24 +122,6 @@ define(
 			},
 			/**
 			 * <pre>
-			 * For the browser does not support CSS3.0.
-			 * Adds element nodes.
-			 * There have ripple and button animation with ajvascript.
-			 * </pre>
-			 * @protected
-			 * @param {object} args Constructor arguments.
-			 * @returns none.
-			 */
-			_render: function(/* object */ args) {
-				this._super(args);
-
-				if (Urushi.hasTransitionSupport()) {
-					return;
-				}
-				$(this.rootNode).find('.toggle').replaceWith(transitionUnit);
-			},
-			/**
-			 * <pre>
 			 * If the browser does not support CSS3.0, it adds callback function to input click event.
 			 * It sets initial value to input.
 			 * </pre>
@@ -157,127 +130,7 @@ define(
 			 * @returns none.
 			 */
 			initOption: function(/* object */ args) {
-				if (!Urushi.hasTransitionSupport()) {
-					Urushi.addEvent(this.inputNode, 'click', this, '_onClick');
-				}
-
 				this.setValue(args.checked);
-			},
-			/**
-			 * <pre>
-			 * For the browser does not support CSS3.0.
-			 * Runs toggle and ripple animation with javascript.
-			 * </pre>
-			 * @protected
-			 * @returns none.
-			 */
-			_onClick: function() {
-				var rippleCounter = 0,
-					rippleScale = CONSTANTS.RIPPLE_SCALE_MIN,
-					rippleOpacity = materialConfig.DEFAULT_VALUE_RIPPLE_OPACITY_MIN,
-					checkedBackgroundColor,
-					checkedButtonColor,
-					tmpCheckedBackgroundTag,
-					tmpCheckedButtonTag,
-					notCheckedBackgroundColor,
-					notCheckedButtonColor,
-					tmpNotCheckedBackgroundTag,
-					tmpNotCheckedButtonTag,
-					ripple = function(/* object */ node, /* number */ duration) {
-						if (rippleCounter * CONSTANTS.INTERVAL < duration) {
-							setTimeout(function() {
-								rippleScale += CONSTANTS.RIPPLE_SCALE_MAX * CONSTANTS.INTERVAL / duration;
-								rippleOpacity += materialConfig.DEFAULT_VALUE_RIPPLE_OPACITY_MAX *
-										CONSTANTS.INTERVAL /
-										duration;
-								$(node).css({
-									'msTransform': 'scale(' + rippleScale + ')',
-									'opacity': rippleOpacity
-								});
-								rippleCounter++;
-								ripple(node, duration);
-							}, CONSTANTS.INTERVAL);
-						} else {
-							$(node).animate({
-								'msTransform': 'scale(' + CONSTANTS.RIPPLE_SCALE_MIN + ')',
-								'opacity': materialConfig.DEFAULT_VALUE_RIPPLE_OPACITY_MIN
-							}, duration, function() {
-								$(node).parents('.togglebutton').find('input').prop('disabled', false);
-							});
-						}
-					};
-
-				if (Urushi.hasTransitionSupport()) {
-					return;
-				}
-
-				if (this.inputNode.hasAttribute('disabled')) {
-					return;
-				}
-
-				if (this.inputNode.checked) {
-					if (this.rootNode.classList.contains('togglebutton') && 1 !== this.rootNode.classList.length) {
-						tmpCheckedBackgroundTag = document.createElement('div');
-						tmpCheckedBackgroundTag.classList.add('toggle-transition-unit-tmp');
-						this.inputNode.appendChild(tmpCheckedBackgroundTag);
-
-						tmpCheckedButtonTag = document.createElement('div');
-						tmpCheckedButtonTag.classList.add('toggle-button-unit-tmp');
-						this.inputNode.appendChild(tmpCheckedButtonTag);
-
-						checkedBackgroundColor = $(this.inputNode).
-								find('.toggle-transition-unit-tmp').
-								css('background-color');
-						checkedButtonColor = $(this.inputNode).
-								find('.toggle-button-unit-tmp').
-								css('background-color');
-						$(tmpCheckedBackgroundTag).remove();
-						$(tmpCheckedButtonTag).remove();
-					}
-
-					$(this.buttonNode).animate({
-						left: CONSTANTS.CHECKMARK_POSITION_LEFT,
-						'backgroundColor': checkedButtonColor
-					}, materialConfig.DEFAULT_VALUE_DURATION);
-					$(this.toggleNode).animate({
-						'backgroundColor': checkedBackgroundColor
-					}, materialConfig.DEFAULT_VALUE_DURATION);
-					$(this.rippleNode).animate({
-						left: CONSTANTS.RIPPLE_POSITION_LEFT,
-					}, materialConfig.DEFAULT_VALUE_DURATION, ripple(
-								this.rippleNode,
-								materialConfig.DEFAULT_VALUE_DURATION
-							));
-				} else {
-					tmpNotCheckedBackgroundTag = document.createElement('div');
-					tmpNotCheckedButtonTag = document.createElement('div');
-					tmpNotCheckedBackgroundTag.className = 'toggle-transition-unit';
-					tmpNotCheckedButtonTag.className = 'toggle-button-unit';
-					this.inputNode.appendChild(tmpNotCheckedBackgroundTag);
-					this.inputNode.appendChild(tmpNotCheckedButtonTag);
-					notCheckedBackgroundColor = $(this.inputNode).
-							find('.toggle-transition-unit').
-							css('background-color');
-					notCheckedButtonColor = $(this.inputNode).
-							find('.toggle-button-unit').
-							css('background-color');
-					$(tmpNotCheckedBackgroundTag).remove();
-					$(tmpNotCheckedButtonTag).remove();
-
-					$(this.buttonNode).animate({
-						left: CONSTANTS.CHECKMARK_POSITION_RIGHT,
-						'backgroundColor': notCheckedButtonColor
-					}, materialConfig.DEFAULT_VALUE_DURATION);
-					$(this.toggleNode).animate({
-						'backgroundColor': notCheckedBackgroundColor
-					}, materialConfig.DEFAULT_VALUE_DURATION);
-					$(this.rippleNode).animate({
-						left: CONSTANTS.RIPPLE_POSITION_RIGHT
-					}, materialConfig.DEFAULT_VALUE_DURATION, ripple(
-							this.rippleNode,
-							materialConfig.DEFAULT_VALUE_DURATION
-						));
-				}
 			},
 			/**
 			 * <pre>
@@ -296,13 +149,6 @@ define(
 			 */
 			_attachNode: function() {
 				this.inputNode = this.rootNode.getElementsByTagName('input')[0];
-
-				if (Urushi.hasTransitionSupport()) {
-					return;
-				}
-				this.toggleNode = this.rootNode.getElementsByClassName('toggle-transition-unit')[0];
-				this.rippleNode = this.rootNode.getElementsByClassName('toggle-ripple-unit')[0];
-				this.buttonNode = this.rootNode.getElementsByClassName('toggle-button-unit')[0];
 			},
 			/**
 			 * @see {@link module:_Base}#_getId
@@ -354,8 +200,6 @@ define(
 					return;
 				}
 				this.inputNode.checked = is;
-
-				this._onClick();
 			},
 			/**
 			 * <pre>
@@ -365,20 +209,6 @@ define(
 			 */
 			clear: function() {
 				this.setValue(false);
-			},
-			/**
-			 * <pre>
-			 * Discarding of instance.
-			 * Delete callback function.
-			 * </pre>
-			 * @returns none.
-			 */
-			destroy: function() {
-				if (!Urushi.hasTransitionSupport()) {
-					Urushi.removeEvent(this.inputNode, 'click', this, '_onClick');
-				}
-
-				this._super();
 			}
 		});
 	}
